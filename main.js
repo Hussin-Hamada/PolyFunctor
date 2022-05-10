59fa09f7a036459cce738a3ba80a83cbeebf432e
EyadOmar <eiad.omar88@gmail.com>
2022-05-10 15:45:38 +0200

    login/signUp UI
    user login/signUp UI added

--- main.js
+++ main.js
@@ -1,7 +1,55 @@
-let form = document.querySelector("form");
+// *********************Login/createAccount************************
+// *****************************************************************
+const profile = document.getElementById("profile-icon");
+profile.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.querySelector(".login-container").classList.remove("display-none");
+  document.querySelector(".cover").classList.remove("display-none");
+  console.log(document.querySelector(".login-container").classList);
+  console.log(document.querySelector(".cover"));
+});
+
+const xLogin = document.getElementById("login-container-x-mark");
+xLogin.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.querySelector(".login-container").classList.add("display-none");
+  document.querySelector(".cover").classList.add("display-none");
+});
+
+const loginForm = document.getElementById("login");
+const createAccountForm = document.getElementById("createAccount");
+const createAccountLink = document.getElementById("linkCreateAccount");
+createAccountLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  loginForm.classList.add("display-none");
+  createAccountForm.classList.remove("display-none");
+});
+const loginLink = document.getElementById("linkLogin");
+loginLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  loginForm.classList.remove("display-none");
+  createAccountForm.classList.add("display-none");
+});
+
+function setFormMsg(formElement, type, message) {
+  const msgElement = formElement.querySelector(".form-msg");
+
+  msgElement.textContent = message;
+  msgElement.classList.remove("form-msg-error", "form-msg-success");
+  msgElement.classList.add(`form-msg-${type}`);
+}
+
+setFormMsg(loginForm, "success", "Your are logged in!");
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  setFormMsg(loginForm, "error", "Invalid username/password combination.");
+});
 
 // *********************Input UI************************
 // *******************************************************
+const methodsForm = document.getElementById("methodsForm");
 
 // Condition Iterations or error
 let conditionValue = document.getElementById("conditionValue");
@@ -67,10 +115,12 @@ methodSelect.addEventListener("change", () => {
 
 // *********************Calculations*********************
 // *******************************************************
-
-form.addEventListener("submit", (e) => {
+let solutionSection = document.querySelector(".solution");
+console.log(solutionSection);
+methodsForm.addEventListener("submit", (e) => {
   e.preventDefault();
-
+  solutionSection.classList.remove("display-none");
+  document.getElementById("root").scrollIntoView({ behavior: "smooth" });
   let condition = conditionSelect.value;
 
   let i = 0;
